d5f23e865b6d4610e829c650673b566901d5289f
Covenant <cfaluyi@gmail.com>
2021-04-08 17:06:45 -0400

    New app.js that incorporates two pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import './App.css';
 
 import Threshold from './components/threshold';
 import ColorPicker from './components/colorPicker'
+import React, { useState } from "react";
+import KeywordPage from './components/KeywordPage'
 
 function App() {
-  return (
-    <div className="App">
-      <Threshold/>
-      <ColorPicker colors = {['#FF4747', '#474FFF', '#12A43B', '#E78225', '#A262E2']}/>
+
+  const [page, setPage] = useState('homePage');
+
+  return ( 
+    <div>
+      {page == 'homePage' ?  <div className="App">
+        <Threshold/>
+        <ColorPicker colors = {['#FF4747', '#474FFF', '#12A43B', '#E78225', '#A262E2']}/>
+        <u onClick = {()=> setPage('keywordPage')} style={{cursor: 'pointer'}} className='keyword-click '>Add/Edit Keywords!</u>
+      </div> : 
+      <KeywordPage cancelClick = {()=> setPage('homePage')}/>
+      }
+     
     </div>
   );
 }
