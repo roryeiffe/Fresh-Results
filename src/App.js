785e7e5f63a4a314624c18fc5d0b2575b2359333
Abdul-Muiz Yusuff <58189969+sacrael@users.noreply.github.com>
2021-04-08 16:19:49 -0400

    Added toggle button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 
 import Threshold from './components/threshold';
-import ColorPicker from './components/colorPicker'
+import ColorPicker from './components/colorPicker';
+import ToggleButton from './components/toggleButton';
 
 function App() {
   return (
     <div className="App">
-      <Threshold/>
-      <ColorPicker colors = {['#FF4747', '#474FFF', '#12A43B', '#E78225', '#A262E2']}/>
+      <Threshold />
+      <ColorPicker colors={['#FF4747', '#474FFF', '#12A43B', '#E78225', '#A262E2']} />
+      <div style={{
+        marginTop: '20px'
+      }}>
+        <ToggleButton
+          onToggle={(toggleState) => {
+            console.log(`Button toggled! (new state => ${toggleState})`);
+          }}
+        />
+      </div>
     </div>
   );
 }
