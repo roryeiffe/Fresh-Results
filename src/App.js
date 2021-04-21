b7b6e4a32089368123da8ffbaf6b8c3df572f575
roryeiffe <eiffer@rpi.edu>
2021-04-21 16:00:58 -0400

    Commented out some unused variables so that the tests will pass and the branch can merge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
-import MoreInfo, { MoreInfoWrapper } from './components/moreInfo'
+import  { MoreInfoWrapper } from './components/moreInfo'
 import Threshold from './components/threshold';
 import ColorPicker from './components/colorPicker';
 import ToggleButton from './components/toggleButton';
 
-import styles from "./app.module.css";
+// import styles from "./app.module.css";
 
 /*global chrome*/
 
