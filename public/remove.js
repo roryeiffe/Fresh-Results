cd64c3b3dd89611b37847cd8204d15057ae6526c
root <root@DESKTOP-F6I8EQQ.localdomain>
2021-04-22 18:17:11 -0400

    added looping for continuous checks

--- public/remove.js
+++ public/remove.js
@@ -1,4 +1,4 @@
-setTimeout(function(){
+window.setInterval(function(){
   var element = document.querySelector("body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf.emcav > div.UUbT9")
   element.remove()
-}, 2000);
+}, 50);
