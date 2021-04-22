a28f342ae318d2da9ddf72ea816eb817a79efa98
root <root@DESKTOP-F6I8EQQ.localdomain>
2021-04-22 17:41:24 -0400

    autosuggest block

---
+++ public/remove.js
@@ -0,0 +1,4 @@
+setTimeout(function(){
+  var element = document.querySelector("body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf.emcav > div.UUbT9")
+  element.remove()
+}, 2000);
