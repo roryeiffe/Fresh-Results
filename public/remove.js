830d4336a5e916488067ce3fa54f499ab7ca9ed1
BreadGarLick <buplasma@gmail.com>
2021-04-24 01:22:20 -0400

    fixed spacing

--- public/remove.js
+++ public/remove.js
@@ -1,7 +1,6 @@
-
-
 //Base google is the autocomlete from initially searching through google.com
 var base_google = "body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf.emcav > div.UUbT9"
+
 //while search is the autocomplete that shows up after you have searched one
 //thing on google.com, and change your query
 var while_search_google =  "#tsf > div:nth-child(1) > div.A8SBwf.emcav > div.UUbT9"
